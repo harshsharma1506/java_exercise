723492e6b5b21d5fb5d9b2592228d68da55cdccd
Harsh Sharma <94862735+harshsharma1506@users.noreply.github.com>
2024-10-10 23:34:13 +0530

    Create test_enum.java

---
+++ src/test_enum.java
@@ -0,0 +1,13 @@
+enum test {
+	LOW,
+	Tre,
+	HELLO
+}
+class test_cls{
+	public static void test_meth( ) {
+		System.out.println(test.Tre);
+	}
+    public static void main(String[] args){
+    	test_meth();
+    }
+}
