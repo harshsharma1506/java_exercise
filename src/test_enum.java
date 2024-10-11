56deb0b358f7848ec32900f57a774bf29b999c8c
Harsh Sharma <94862735+harshsharma1506@users.noreply.github.com>
2024-10-11 22:05:49 +0530

    Update test_enum.java

--- src/test_enum.java
+++ src/test_enum.java
@@ -9,5 +9,10 @@ class test_cls{
 	}
     public static void main(String[] args){
     	test_meth();
+        // testing the 4 types of constants :) 
+	final String a = "Harsh"; // double apostrophe for string 
+	final char b = 'H'; // single apostrophe for character
+	final int c = 1;  // integer 
+	final double d = 2.4; // real 
     }
 }
