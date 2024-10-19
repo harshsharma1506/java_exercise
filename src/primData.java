1fef6dfd10d6964053c6a4529f9eec36e815f0b7
Harsh Sharma <94862735+harshsharma1506@users.noreply.github.com>
2024-10-19 15:23:51 +0530

    Create primData.java

---
+++ src/primData.java
@@ -0,0 +1,19 @@
+class test{
+	public static void main( String[] args) {
+/** These are all primitive data types , first we are going towards INT, as int has 4 types - byte , short, int , long 
+ *  then we have floating type as float and double, finally we will have characters and boolean */
+		byte tst = 2; // stores from - 128 to 127
+		short tst2 = 4500; // stores from -32,768 to 32,767
+		int tst_3 = 456690000; // stores from -2147483648 to 2147483647
+		long tst_4 = 50000000; // 64 bit two complement integer goes from -2 raise to 63 to 2 raise to 63 - 1
+/*
+ * Below are floating types , float > decimal 		
+ */
+		float tst_5 = 5.88889f; // end with an f  3.4e-038 to 3.4e+038
+		double tst_6 = 5.8999d; // end with a d 1.7e-308 to 1.7e+308 
+/*
+ * Below are character and boolean  
+ */
+		char tst_7 = 'C';
+		boolean tst_8 = true;
+	}
